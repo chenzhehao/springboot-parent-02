2dd3e002ac1d65b9162b2c5819cbd585fa0e3356
zhehao.chen <852347470@qq.com>
2024-10-16 16:41:51 +0800

    test 1016 001

--- springboot-jdk18-02/src/main/java/com/czh/springboot/JDK18Test.java
+++ springboot-jdk18-02/src/main/java/com/czh/springboot/JDK18Test.java
@@ -106,8 +106,9 @@ public class JDK18Test {
 		for (int i = 0; i < a.length - 1; i++) {
 			min_index = i;
 			for (int j = i + 1; j < a.length; j++) {
-				if (a[min_index] > a[j])
+				if (a[min_index] > a[j]){
 					min_index = j;
+				}
 			}
 			if (min_index != i) {
 				int temp = a[min_index];
