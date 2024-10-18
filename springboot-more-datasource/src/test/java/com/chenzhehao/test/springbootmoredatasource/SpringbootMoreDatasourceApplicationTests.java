8a1a705d23e56c90b501e8fd2a2965e56221da2f
zhehao.chen <852347470@qq.com>
2024-10-18 10:07:20 +0800

    first commit 1018

---
+++ springboot-more-datasource/src/test/java/com/chenzhehao/test/springbootmoredatasource/SpringbootMoreDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenzhehao.test.springbootmoredatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMoreDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
