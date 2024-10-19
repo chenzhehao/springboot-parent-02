2981a34c9fa33683cbb74c5dbaef33e50f19aa30
zhehao.chen <852347470@qq.com>
2024-10-19 17:35:45 +0800

    datasource dynamic 1019002

---
+++ springboot-more-datasource-dynamic/src/main/java/com/chenzhehao/test/springbootmoredatasourcedynamic/SpringbootMoreDatasourceDynamicApplication.java
@@ -0,0 +1,13 @@
+package com.chenzhehao.test.springbootmoredatasourcedynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMoreDatasourceDynamicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMoreDatasourceDynamicApplication.class, args);
+	}
+
+}
---
+++ springboot-more-datasource-dynamic/src/test/java/com/chenzhehao/test/springbootmoredatasourcedynamic/SpringbootMoreDatasourceDynamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenzhehao.test.springbootmoredatasourcedynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMoreDatasourceDynamicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
