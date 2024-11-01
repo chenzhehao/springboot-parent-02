0ad18831ed0474142a3805c76ddee539718c8c4b
zhehao.chen <852347470@qq.com>
2024-11-01 14:54:25 +0800

    test 1101001 init

--- spring-jvm-02/src/main/java/com/czh/springboot/configure/listener/MyApplicationListenerStarted.java
+++ spring-jvm-02/src/main/java/com/czh/springboot/configure/listener/MyApplicationListenerStarted.java
@@ -1,12 +1,11 @@
 package com.czh.springboot.configure.listener;
 
 import org.springframework.boot.context.event.ApplicationStartedEvent;
-import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyApplicationListenerStarting implements ApplicationListener<ApplicationStartedEvent> {
+public class MyApplicationListenerStarted implements ApplicationListener<ApplicationStartedEvent> {
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
